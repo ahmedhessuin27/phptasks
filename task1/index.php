e1f9e00ff15b26e364e97af847db3c5b74080bb0
ahmedhessuin27 <am8150688@gmail.com>
2023-09-12 20:50:38 +0200

    finished

---
+++ task1/index.php
@@ -0,0 +1,21 @@
+<?php
+$name ="ahmed";
+echo"my name is $name";
+echo"<br>";
+echo'my name is ahmed';
+echo"<br>";
+$firstname ='ahmed';
+$lastname ='hessuin';
+echo'my fullname is '. $firstname . ' '. $lastname . ' and my age is 21';
+echo '<br>';
+echo "my fullname is $firstname $lastname and my age is 21";
+
+// 1. In Echo, we can pass multiple arguments separated by commas.	
+//    In Print, we cannot pass multiple arguments.
+
+// 2.Echo is fast as compared to the print statement.
+// 	print is slow as compared to the echo statement
+
+// 3. 	Echo never returns any value.
+// 	print always returns the integer value that is 1.
+?>
